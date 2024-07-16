tanaypf9/pf9-requirements

Create a merge-constraints tool
Currently we have a tool call generate-constraints that operates like so:

foreach supplied python_version
  Create a venv
  Install global-requirements in that venv
  generate a pip frezze

foreach freeze combine
  Combine into a single set of constrains.

Use the 'proposal' framework in project-config to generate a gerrit
change wit the new constraints

There is also the ability to simulate a python_version, but this isn't
accurate.  The major limitation is we can only generate accurate
constraints for python versions supplied by the distribution.

This change and the following changes will alter that process.

Step one: (this change)
 Move the generation and combination code into a library
 Create a new tool call merge-constraints that will take individually
   create freeze outputs and combine them to generate a single
   constraints file.

This is done without disrupting the existing generate-constratints tool

Step two:

Add a multinode job that will use ansible to generate a freeze for all
available python versions.  This is multinode so we can add new OSs and
python releases somewhat trivially.

At the time of writing:
  Use ubuntu-focal to generate py3.8 and py3.9 freezees
  Use ubuntu-jammy to generate py3.10 and py3.11 freezees

Step three:

Add another job that will use ansible to take the freezes generated in
step two and combine them into a single constraints file

Step four, and beyond: (to be written)

Add the new jobs into the proposal framework alongside the existing
generate-constraints tool for comparison.  Ensure that the bulk of the
tools and job definitions are in openstack/requirements with only the
proposal logic in openstack/project-config, it needs to be there to
access the zuul secrets.Disable and remove the older tool once we're
happy with the new tool.

Change-Id: If4f9d1adb9c259dc0fbe067ff6cf9e007d697acf

openstack_requirements/cmds/generate.py
@@ -11,222 +11,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import copy
-from datetime import datetime
-import hashlib
 import optparse
-import os.path
-import shutil
-import subprocess
 import sys
-import tempfile
 
-import fixtures
-
-from openstack_requirements import requirement
-
-
-def _parse_freeze(text):
-    """Parse a freeze into structured data.
-
-    :param text: The output from a pip freeze command.
-    :return: A list of (package, version) tuples.
-    """
-    result = []
-    for line in text.splitlines():
-        line = line.strip()
-        if line.startswith('-'):
-            raise Exception("Irregular line: %s" % line)
-        if line.startswith('#'):
-            continue
-        if not line:
-            continue
-        package, version = line.split('==')[:2]
-        result.append((package, version))
-    return result
-
-
-def _freeze(requirements, python):
-    """Generate a frozen install from requirements.
-
-    A constraints file is the result of installing a set of requirements and
-    then freezing the result. We currently special case pip and setuptools
-    as pip does, excluding them from the set. We may however want to revisit
-    this in future if releases of those things break our gate.
-
-    In principle we should determine this by introspecting all the packages
-    transitively, since we need to deal wit environment markers....
-    but thats reimplementing a large chunk of pip (and since pip doesn't
-    resolve yet, differently too). For now, we take a list of Python
-    executables to test under, and then union the results. This is in fact the
-    key difference between a constraints file and a requirements file: we're
-    not triggering installation, so we can and will list packages that are
-    not relevant to e.g. Python3 in the constraints output.
-
-    :param requirements: The path to a requirements file to use when generating
-        the constraints.
-    :param python: A Python binary to use. E.g. /usr/bin/python3
-    :return: A tuple (python_version, list of (package, version)'s)
-    """
-    output = []
-    try:
-        version_out = subprocess.check_output(
-            [python, "--version"], stderr=subprocess.STDOUT)
-        output.append(version_out)
-        version_all = version_out.decode('utf-8').split()[1]
-        version = '.'.join(version_all.split('.')[:2])
-        with fixtures.TempDir() as temp:
-            output.append(subprocess.check_output(
-                [python, '-m', 'venv', temp.path]))
-            pip_bin = os.path.join(temp.path, 'bin', 'pip')
-            output.append(subprocess.check_output(
-                [pip_bin, 'install', '-U', 'pip', 'setuptools', 'wheel']))
-            output.append(subprocess.check_output(
-                [pip_bin, 'install', '-r', requirements]))
-            freeze = subprocess.check_output(
-                [pip_bin, 'freeze'])
-            output.append(freeze)
-            return (version, _parse_freeze(freeze.decode('utf-8')))
-    except Exception as exc:
-        if isinstance(exc, subprocess.CalledProcessError):
-            output.append(exc.output)
-        raise Exception(
-            "Failed to generate freeze: %s %s" % (
-                b'\n'.join(output).decode('utf-8'),
-                exc,
-            )
-        )
-    finally:
-        # log job results
-        with tempfile.NamedTemporaryFile(
-            mode='w+b',
-            prefix=datetime.now().strftime(
-                'generate-constraints-%Y_%m_%d_%H_%M-'
-            ),
-            delete=False,
-        ) as fh:
-            fh.write(b'\n'.join(output))
-
-
-def _combine_freezes(freezes, blacklist=None):
-    """Combine multiple freezes into a single structure.
-
-    This deals with the variation between different python versions by
-    generating environment markers when different pythons need different
-    versions of a dependency.
-
-    :param freezes: A list of (python_version, frozen_requirements) tuples.
-    :param blacklist: An iterable of package names to exclude. These packages
-        won't be included in the output.
-    :return: A list of '\n' terminated lines for a requirements file.
-    """
-    packages = {}  # {package : {version : [py_version]}}
-    excludes = frozenset((requirement.canonical_name(s)
-                          for s in blacklist) if blacklist else ())
-    reference_versions = []
-    for py_version, freeze in freezes:
-        if py_version in reference_versions:
-            raise Exception("Duplicate python %s" % py_version)
-
-        reference_versions.append(py_version)
-        for package, version in freeze:
-            packages.setdefault(
-                package, {}).setdefault(version, []).append(py_version)
-
-    for package, versions in sorted(packages.items()):
-        if package.lower() in excludes:
-            continue
-
-        if len(versions) > 1:
-            # markers for packages with multiple versions - we use python
-            # version ranges for these
-            for idx, (version, py_versions) in enumerate(sorted(versions.items())):  # noqa: E501
-                if idx == 0:  # lower-bound
-                    marker = f"python_version<='{py_versions[-1]}'"
-                elif idx + 1 != len(versions):  # intermediate version(s)
-                    marker = f"python_version>='{py_versions[0]}',<={py_versions[-1]}"  # noqa: E501
-                else:  # upper-bound
-                    marker = f"python_version>='{py_versions[0]}'"
-
-                yield f'{package}==={version};{marker}\n'
-        elif list(versions.values())[0] != reference_versions:
-            # markers for packages with a single version - these are usually
-            # version specific so we use strict python versions for these
-            for idx, (version, py_versions) in enumerate(sorted(versions.items())):  # noqa: E501
-                for py_version in sorted(py_versions):
-                    marker = f"python_version=='{py_version}'"
-                    yield f'{package}==={version};{marker}\n'
-        else:
-            # no markers
-            yield f'{package}==={list(versions)[0]}\n'
-
-
-def _clone_versions(freezes, options):
-    for freeze_data in freezes:
-        versions = [v for v, d in freezes]
-        version, freeze = freeze_data
-        if version in options.version_map:
-            for dst_version in sorted(options.version_map[version]):
-                if dst_version not in versions:
-                    print("Duplicating %s freeze to %s" %
-                          (version, dst_version), file=sys.stderr)
-                    freezes.append((dst_version, copy.copy(freeze)))
-
-
-# -- untested UI glue from here down.
-
-def _validate_options(options):
-    """Check that options are valid.
-
-    :param options: The optparse options for this program.
-    """
-    if not options.pythons:
-        raise Exception("No Pythons given - see -p.")
-    for python in options.pythons:
-        if not shutil.which(python):
-            raise Exception(
-                "Python %(python)s not found." % dict(python=python))
-    if not options.requirements:
-        raise Exception("No requirements file specified - see -r.")
-    if not os.path.exists(options.requirements):
-        raise Exception(
-            "Requirements file %(req)s not found."
-            % dict(req=options.requirements))
-    if options.blacklist and not os.path.exists(options.blacklist):
-        raise Exception(
-            "Blacklist file %(path)s not found."
-            % dict(path=options.blacklist))
-    version_map = {}
-    for map_entry in options.version_map:
-        if ':' not in map_entry:
-            raise Exception(
-                "Invalid version-map entry %(map_entry)s"
-                % dict(map_entry=map_entry))
-        src, dst = map_entry.split(':')
-        version_map.setdefault(src, set())
-        version_map[src].add(dst)
-    options.version_map = version_map
-
-
-def _parse_blacklist(path):
-    """Return the strings from path if it is not None."""
-    if path is None:
-        return []
-    with open(path, 'rt') as f:
-        return [line.strip() for line in f]
-
-
-def _make_sort_key(line):
-    """Produce a key that is unlikely to place similar values together.
-
-    We want to avoid sorting all of the oslo libraries together (or
-    all of the python-*client libraries) so when we do batch releases
-    we do not have merge conflicts in the individual patches updating
-    the constraints.
-
-    """
-    dep = line.partition('=')[0].encode('utf-8')
-    return hashlib.sha1(dep).digest()
+from openstack_requirements.cmdlib import _clone_versions
+from openstack_requirements.cmdlib import _combine_freezes
+from openstack_requirements.cmdlib import _freeze
+from openstack_requirements.cmdlib import _make_sort_key
+from openstack_requirements.cmdlib import _parse_blacklist
+from openstack_requirements.cmdlib import _validate_options
 
 
 def main(argv=None, stdout=None):

openstack_requirements/cmds/merge_constraints.py
@@ -1,0 +1,42 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import sys
+
+from pprint import pprint  # noqa
+
+from openstack_requirements.cmdlib import _combine_freezes
+from openstack_requirements.cmdlib import _make_sort_key
+from openstack_requirements.cmdlib import _parse_blacklist
+from openstack_requirements.cmdlib import _parse_freeze
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--blacklist",
+                        help="Filename of a list of package names to exclude.")
+    parser.add_argument("-c", "--constraints", action="append", help="")
+    args = parser.parse_args(argv or sys.argv[1:])
+    freezes = []
+    for constraint in args.constraints:
+        (pyver, file) = constraint.split(':', 1)
+        with open(file) as f:
+            freeze = f.read()
+        freezes.append((pyver, _parse_freeze(freeze)))
+    blacklist = _parse_blacklist(args.blacklist)
+    frozen = [*sorted(_combine_freezes(freezes, blacklist),
+                      key=_make_sort_key)]
+    sys.stdout.writelines(frozen)
+    sys.stdout.flush()
+    return 0

sourabh-pf9/openstack-requirements

openstack_requirements/cmds/generate.py
@@ -11,222 +11,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import copy
-from datetime import datetime
-import hashlib
 import optparse
-import os.path
-import shutil
-import subprocess
 import sys
-import tempfile
 
-import fixtures
-
-from openstack_requirements import requirement
-
-
-def _parse_freeze(text):
-    """Parse a freeze into structured data.
-
-    :param text: The output from a pip freeze command.
-    :return: A list of (package, version) tuples.
-    """
-    result = []
-    for line in text.splitlines():
-        line = line.strip()
-        if line.startswith('-'):
-            raise Exception("Irregular line: %s" % line)
-        if line.startswith('#'):
-            continue
-        if not line:
-            continue
-        package, version = line.split('==')[:2]
-        result.append((package, version))
-    return result
-
-
-def _freeze(requirements, python):
-    """Generate a frozen install from requirements.
-
-    A constraints file is the result of installing a set of requirements and
-    then freezing the result. We currently special case pip and setuptools
-    as pip does, excluding them from the set. We may however want to revisit
-    this in future if releases of those things break our gate.
-
-    In principle we should determine this by introspecting all the packages
-    transitively, since we need to deal wit environment markers....
-    but thats reimplementing a large chunk of pip (and since pip doesn't
-    resolve yet, differently too). For now, we take a list of Python
-    executables to test under, and then union the results. This is in fact the
-    key difference between a constraints file and a requirements file: we're
-    not triggering installation, so we can and will list packages that are
-    not relevant to e.g. Python3 in the constraints output.
-
-    :param requirements: The path to a requirements file to use when generating
-        the constraints.
-    :param python: A Python binary to use. E.g. /usr/bin/python3
-    :return: A tuple (python_version, list of (package, version)'s)
-    """
-    output = []
-    try:
-        version_out = subprocess.check_output(
-            [python, "--version"], stderr=subprocess.STDOUT)
-        output.append(version_out)
-        version_all = version_out.decode('utf-8').split()[1]
-        version = '.'.join(version_all.split('.')[:2])
-        with fixtures.TempDir() as temp:
-            output.append(subprocess.check_output(
-                [python, '-m', 'venv', temp.path]))
-            pip_bin = os.path.join(temp.path, 'bin', 'pip')
-            output.append(subprocess.check_output(
-                [pip_bin, 'install', '-U', 'pip', 'setuptools', 'wheel']))
-            output.append(subprocess.check_output(
-                [pip_bin, 'install', '-r', requirements]))
-            freeze = subprocess.check_output(
-                [pip_bin, 'freeze'])
-            output.append(freeze)
-            return (version, _parse_freeze(freeze.decode('utf-8')))
-    except Exception as exc:
-        if isinstance(exc, subprocess.CalledProcessError):
-            output.append(exc.output)
-        raise Exception(
-            "Failed to generate freeze: %s %s" % (
-                b'\n'.join(output).decode('utf-8'),
-                exc,
-            )
-        )
-    finally:
-        # log job results
-        with tempfile.NamedTemporaryFile(
-            mode='w+b',
-            prefix=datetime.now().strftime(
-                'generate-constraints-%Y_%m_%d_%H_%M-'
-            ),
-            delete=False,
-        ) as fh:
-            fh.write(b'\n'.join(output))
-
-
-def _combine_freezes(freezes, denylist=None):
-    """Combine multiple freezes into a single structure.
-
-    This deals with the variation between different python versions by
-    generating environment markers when different pythons need different
-    versions of a dependency.
-
-    :param freezes: A list of (python_version, frozen_requirements) tuples.
-    :param denylist: An iterable of package names to exclude. These packages
-        won't be included in the output.
-    :return: A list of '\n' terminated lines for a requirements file.
-    """
-    packages = {}  # {package : {version : [py_version]}}
-    excludes = frozenset((requirement.canonical_name(s)
-                          for s in denylist) if denylist else ())
-    reference_versions = []
-    for py_version, freeze in freezes:
-        if py_version in reference_versions:
-            raise Exception("Duplicate python %s" % py_version)
-
-        reference_versions.append(py_version)
-        for package, version in freeze:
-            packages.setdefault(
-                package, {}).setdefault(version, []).append(py_version)
-
-    for package, versions in sorted(packages.items()):
-        if package.lower() in excludes:
-            continue
-
-        if len(versions) > 1:
-            # markers for packages with multiple versions - we use python
-            # version ranges for these
-            for idx, (version, py_versions) in enumerate(sorted(versions.items())):  # noqa: E501
-                if idx == 0:  # lower-bound
-                    marker = f"python_version<='{py_versions[-1]}'"
-                elif idx + 1 != len(versions):  # intermediate version(s)
-                    marker = f"python_version>='{py_versions[0]}',<={py_versions[-1]}"  # noqa: E501
-                else:  # upper-bound
-                    marker = f"python_version>='{py_versions[0]}'"
-
-                yield f'{package}==={version};{marker}\n'
-        elif list(versions.values())[0] != reference_versions:
-            # markers for packages with a single version - these are usually
-            # version specific so we use strict python versions for these
-            for idx, (version, py_versions) in enumerate(sorted(versions.items())):  # noqa: E501
-                for py_version in sorted(py_versions):
-                    marker = f"python_version=='{py_version}'"
-                    yield f'{package}==={version};{marker}\n'
-        else:
-            # no markers
-            yield f'{package}==={list(versions)[0]}\n'
-
-
-def _clone_versions(freezes, options):
-    for freeze_data in freezes:
-        versions = [v for v, d in freezes]
-        version, freeze = freeze_data
-        if version in options.version_map:
-            for dst_version in sorted(options.version_map[version]):
-                if dst_version not in versions:
-                    print("Duplicating %s freeze to %s" %
-                          (version, dst_version), file=sys.stderr)
-                    freezes.append((dst_version, copy.copy(freeze)))
-
-
-# -- untested UI glue from here down.
-
-def _validate_options(options):
-    """Check that options are valid.
-
-    :param options: The optparse options for this program.
-    """
-    if not options.pythons:
-        raise Exception("No Pythons given - see -p.")
-    for python in options.pythons:
-        if not shutil.which(python):
-            raise Exception(
-                "Python %(python)s not found." % dict(python=python))
-    if not options.requirements:
-        raise Exception("No requirements file specified - see -r.")
-    if not os.path.exists(options.requirements):
-        raise Exception(
-            "Requirements file %(req)s not found."
-            % dict(req=options.requirements))
-    if options.denylist and not os.path.exists(options.denylist):
-        raise Exception(
-            "Denylist file %(path)s not found."
-            % dict(path=options.denylist))
-    version_map = {}
-    for map_entry in options.version_map:
-        if ':' not in map_entry:
-            raise Exception(
-                "Invalid version-map entry %(map_entry)s"
-                % dict(map_entry=map_entry))
-        src, dst = map_entry.split(':')
-        version_map.setdefault(src, set())
-        version_map[src].add(dst)
-    options.version_map = version_map
-
-
-def _parse_denylist(path):
-    """Return the strings from path if it is not None."""
-    if path is None:
-        return []
-    with open(path, 'rt') as f:
-        return [line.strip() for line in f]
-
-
-def _make_sort_key(line):
-    """Produce a key that is unlikely to place similar values together.
-
-    We want to avoid sorting all of the oslo libraries together (or
-    all of the python-*client libraries) so when we do batch releases
-    we do not have merge conflicts in the individual patches updating
-    the constraints.
-
-    """
-    dep = line.partition('=')[0].encode('utf-8')
-    return hashlib.sha1(dep).digest()
+from openstack_requirements.cmdlib import _clone_versions
+from openstack_requirements.cmdlib import _combine_freezes
+from openstack_requirements.cmdlib import _freeze
+from openstack_requirements.cmdlib import _make_sort_key
+from openstack_requirements.cmdlib import _parse_denylist
+from openstack_requirements.cmdlib import _validate_options
 
 
 def main(argv=None, stdout=None):

openstack_requirements/cmds/merge_constraints.py
@@ -1,0 +1,42 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import sys
+
+from pprint import pprint  # noqa
+
+from openstack_requirements.cmdlib import _combine_freezes
+from openstack_requirements.cmdlib import _make_sort_key
+from openstack_requirements.cmdlib import _parse_denylist
+from openstack_requirements.cmdlib import _parse_freeze
+
+
+def main(argv=None):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--denylist",
+                        help="Filename of a list of package names to exclude.")
+    parser.add_argument("-c", "--constraints", action="append", help="")
+    args = parser.parse_args(argv or sys.argv[1:])
+    freezes = []
+    for constraint in args.constraints:
+        (pyver, file) = constraint.split(':', 1)
+        with open(file) as f:
+            freeze = f.read()
+        freezes.append((pyver, _parse_freeze(freeze)))
+    denylist = _parse_denylist(args.denylist)
+    frozen = [*sorted(_combine_freezes(freezes, denylist),
+                      key=_make_sort_key)]
+    sys.stdout.writelines(frozen)
+    sys.stdout.flush()
+    return 0